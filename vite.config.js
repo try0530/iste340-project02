d815dcc411d346d678582e76f95a82e1fe3a0874
unknown <s900003terry@gmail.com>
2024-11-25 12:17:51 -0500

    fix base

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/yy9451/iste340/project02/",
+  // base: "/yy9451/iste340/project02/",
+  base: "/",
 });
